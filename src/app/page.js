58ff5d5dad02223a13bd91115b99d90d1a297a6a
shreshkhatri <shresh.khatri@gmail.com>
2023-09-26 22:08:08 +0100

    temporary link used

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,28 @@
 'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Link from 'next/link'
 
 import AppTheme from '@/assets/AppTheme';
 
-
 export default function Home() {
+
 
   return (
     <AppTheme>
-        <Box sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'flex-start',
-          alignItems: 'center',
-          minHeight: '100vh',
-          width: '100%'
-        }}>
-          <Typography textAlign='center'>Hi Welcome to my page</Typography>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        width: '100%',
+      }}>
+        <Link href="/my-cv" style={{textDecoration:'underline'}}>Click here to build your CV</Link>
 
-        </Box>
+      </Box>
     </AppTheme>
   )
 }
