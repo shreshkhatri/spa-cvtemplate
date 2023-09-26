f782713c62d799344278b127edc87da1cd96ce8b
shreshkhatri <shresh.khatri@gmail.com>
2023-09-26 10:28:38 +0100

    auth feature removed temporarily

--- src/app/api/auth/logout.js
+++
@@ -1,6 +0,0 @@
-// pages/api/auth/logout.js
-export default function handler(req, res) {
-    req.logout();
-    res.status(200).json({ message: 'Logged out successfully' });
-  }
-  
--- src/app/api/auth/status.js
+++
@@ -1,5 +0,0 @@
-// pages/api/auth/status.js
-export default function handler(req, res) {
-    res.status(200).json({ isAuthenticated: req.isAuthenticated() });
-  }
-  
